fe82c1f2d48adf24f9450d8a6be8122ca17593f6
novr1512 <you@example.com>
2021-07-13 11:38:58 +0300

    control works

--- webui.js
+++ webui.js
@@ -35,7 +35,7 @@ function initVelocityPublisher() {
     // Init topic object
     cmdVel = new ROSLIB.Topic({
         ros: ros,
-        name: '/cmd_vel',
+        name: '/cmd_vel_mux/input/navi',
         messageType: 'geometry_msgs/Twist'
     });
     // Register publisher within ROS system
@@ -51,7 +51,7 @@ function initVelocityPublisher() {
             // Initialize the teleop.
             teleop = new KEYBOARDTELEOP.Teleop({
                 ros: ros,
-                topic: '/cmd_vel'
+                topic: '/cmd_vel_mux/input/navi'
             });
         }
     
@@ -62,51 +62,6 @@ function initVelocityPublisher() {
         }
     }
 
-    function createJoystick() {
-        // Check if joystick was aready created
-        if (manager == null) {
-            joystickContainer = document.getElementById('joystick');
-            // joystck configuration, if you want to adjust joystick, refer to:
-            // https://yoannmoinet.github.io/nipplejs/
-            var options = {
-                zone: joystickContainer,
-                position: { left: 50 + '%', top: 105 + 'px' },
-                mode: 'static',
-                size: 200,
-                color: '#0066ff',
-                restJoystick: true
-            };
-            manager = nipplejs.create(options);
-            // event listener for joystick move
-            manager.on('move', function (evt, nipple) {
-                // nipplejs returns direction is screen coordiantes
-                // we need to rotate it, that dragging towards screen top will move robot forward
-                var direction = nipple.angle.degree - 90;
-                if (direction > 180) {
-                    direction = -(450 - nipple.angle.degree);
-                }
-                // convert angles to radians and scale linear and angular speed
-                // adjust if youwant robot to drvie faster or slower
-                var lin = Math.cos(direction / 57.29) * nipple.distance * 0.005;
-                var ang = Math.sin(direction / 57.29) * nipple.distance * 0.05;
-                // nipplejs is triggering events when joystic moves each pixel
-                // we need delay between consecutive messege publications to 
-                // prevent system from being flooded by messages
-                // events triggered earlier than 50ms after last publication will be dropped 
-                if (publishImmidiately) {
-                    publishImmidiately = false;
-                    moveAction(lin, ang);
-                    setTimeout(function () {
-                        publishImmidiately = true;
-                    }, 50);
-                }
-            });
-            // event litener for joystick release, always send stop message
-            manager.on('end', function () {
-                moveAction(0, 0);
-            });
-        }
-    }
 
     window.onload = function () {
         // determine robot address automatically
@@ -126,7 +81,6 @@ function initVelocityPublisher() {
         video.src = "http://localhost:8080/stream?topic=/camera/rgb/image_raw&type=mjpeg&quality=80";
         video.onload = function () {
             // joystick and keyboard controls will be available only when video is correctly loaded
-            createJoystick();
             initTeleopKeyboard();
         };
     }
